f85057105c1a20620133c5dd12c942eb12898132
sdwqdwd5 <vul3ejo3>
2019-12-29 22:51:54 -0800

    refactor the move method

--- src/main/java/org/example/tankWar/Tank.java
+++ src/main/java/org/example/tankWar/Tank.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.util.Random;
 
 public class Tank {
+    private static final int MOVE_SPEED = 5;
     private int x;
     private int y;
     private boolean enemy;
@@ -43,39 +44,9 @@ public class Tank {
     }
 
     void move() {
-        if (this.stopped){
-            return;
-        }
-        switch (direction) {
-            case UP:
-                y -= 5;
-                break;
-            case DOWN:
-                y += 5;
-                break;
-            case LEFT:
-                x -= 5;
-                break;
-            case RIGHT:
-                x += 5;
-                break;
-            case LEFT_UP:
-                x -= 5;
-                y -= 5;
-                break;
-            case RIGHT_UP:
-                x += 5;
-                y -= 5;
-                break;
-            case LEFT_DOWN:
-                x -= 5;
-                y += 5;
-                break;
-            case RIGHT_DOWN:
-                x += 5;
-                y += 5;
-                break;
-        }
+        if (this.stopped) return;
+        x += direction.xFactor * MOVE_SPEED;
+        y += direction.yFactor * MOVE_SPEED;
     }
 
     Image getImage() {
