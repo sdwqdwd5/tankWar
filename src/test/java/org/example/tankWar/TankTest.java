0608a39682f9f961bf480a7179de3a7e1f8562eb
sdwqdwd5 <sunny246870@gmail.com>
2020-08-13 05:12:27 -0700

    updated JUnit

--- src/test/java/org/example/tankWar/TankTest.java
+++ src/test/java/org/example/tankWar/TankTest.java
@@ -1,7 +1,14 @@
 package org.example.tankWar;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.alibaba.fastjson.JSON;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 class TankTest {
 
     @org.junit.jupiter.api.Test
@@ -13,5 +20,16 @@ class TankTest {
             Tank enemyTank = new Tank(0, 0, true, direction);
             assertTrue(enemyTank.getImage().getWidth(null) > 0, direction + " has problem.");
         }
+    }
+    @org.junit.jupiter.api.Test
+    void save() throws IOException {
+        String dest = "tmp/game.sav";
+        GameClient.getInstance().save(dest);
+
+        byte[] bytes = Files.readAllBytes(Paths.get(dest));
+        Save save = JSON.parseObject(bytes, Save.class);
+        assertTrue(save.isGameContinued());
+
+        assertEquals(12, save.getEnemyPosition().size());
     }
 }
